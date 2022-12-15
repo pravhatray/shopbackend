f9ae238c1ce6ef9e9f3fea9e60099afbc0c2836f
Pravhat Ray <pravhatray@gmail.com>
2022-12-15 14:20:27 +0530

    Update bookmark.route.js

--- src/Routes/bookmark.route.js
+++ src/Routes/bookmark.route.js
@@ -3,41 +3,27 @@ const BookmarkModel = require("../Models/bookmark.model");
 
 const app = express.Router();
 
-app.get('/',async(req,res)=>{
-  let bookmarked=await BookmarkModel.find();
-  res.send(bookmarked);
-})
 
-app.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    let bookmarkedItem = await BookmarkModel.find({ shopItemId: id }).populate(
-      "shopItemId"
-    );
-    res.status(200).send(bookmarkedItem);
-  } catch (er) {
-    return res.status(404).send({ msg: er });
-  }
-});
+app.post("/", async (req, res) => {
+    try {
+      let bookmarked = new BookmarkModel({ ...req.body });
+      await bookmarked.save();
+      res.status(200).send(bookmarked);
+    } catch (er) {
+      return res.status(500).send({ msg: er.message });
+    }
+  });
+  
+  app.get("/", async (req, res) => {
+    try {
+      let bookmarked = await BookmarkModel.find();
+      res.status(200).send(bookmarked);
+    } catch (er) {
+      return res.status(404).send({ msg: er.message });
+    }
+  });
 
-
-app.post("/:id", async (req, res) => {
-  try {
-    let bookmarkeditem = await BookmarkModel.findOne({
-      shopItemId: req.body.shopItemId,
-    });
-
-    let item = await BookmarkModel.create({
-      ...req.body,
-      shopItemId: req.params.id,
-    });
-    return res.send(item);
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
-
-app.delete("/:id",async(req,res)=>{
+  app.delete("/:id",async(req,res)=>{
     try {
       const id = req.params.id;
       let afterDelete = await BookmarkModel.findByIdAndDelete(id);
