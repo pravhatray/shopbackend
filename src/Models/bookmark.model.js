ee5eb34ff9d5075aaa198d16bcdb18899a03b9b0
Pravhat Ray <pravhatray@gmail.com>
2022-12-15 14:19:39 +0530

    Update bookmark.model.js

--- src/Models/bookmark.model.js
+++ src/Models/bookmark.model.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const bookmarkSchema = new mongoose.Schema(
-  {
-    // Title: { type: String, required: true },
-    // Quantity: { type: Number, required: true },
-    // Priority: { type: String, required: true ,enum:["High","Middle","Low"],default:"High"},
-    // Description: { type: String, required: true },
-    shopItemId:{ type: mongoose.Schema.Types.ObjectId, ref: 'shop', required: true }
-  }
+ {
+    Title: { type: String, required: true },
+    Quantity: { type: Number, required: true },
+    Priority: { type: String, required: true ,enum:["High","Middle","Low"],default:"High"},
+    Description: { type: String, required: true },
+  },
+  { timestamps: true }
   
 );
 
